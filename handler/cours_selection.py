cfb1de23f1f59348152a3515f564de369f510d61
mehdi <mehdiFeghhi>
2022-01-22 19:13:14 +0330

    fix bug

--- handler/cours_selection.py
+++ handler/cours_selection.py
@@ -324,10 +324,11 @@ def update_permitted_course_prof_by(permitted_course_id, professor_id, user_id):
     if professorLinkPresentedCourse is None:
         professorLinkPresentedCourse = ProfessorLinkPresentedCourse(professor_email=professor_id,
                                                                      presentedCourse=find_present_course.course_id)
+        session.add(professorLinkPresentedCourse)
+
     else:
 
         professorLinkPresentedCourse.professor_email = professor_id
-    session.add(professorLinkPresentedCourse)
     permitted_course.professor_id = professor_id
     session.commit()
     return {'Status': 'OK', 'message': 'با موفقیت تغییر اعمال شد.'}
